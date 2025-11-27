4a692e9731a46005506291735c4a3154b8b089b5
l200230180 <l200230180@student.ums.ac.id>
2025-11-27 22:35:28 +0700

    Instalasi Tailwind CSS dan penambahan file konfigurasi manual

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Path untuk scan file JSX/JS
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
